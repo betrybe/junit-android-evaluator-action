c0a45a351f9967e7c125c576b4c6540a4802a7e2
katiacih <katiacih@gmail.com>
2022-04-06 11:33:34 -0300

    test: Reestruturacao de tstes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const { runStepsEvaluator } = require('./src/controller/evaluator')
 
+
+// runStepsEvaluator('./src/test/res/')
+// TODO substituir pelo caminho do xml do projeto Android/Kotlin
 runStepsEvaluator()

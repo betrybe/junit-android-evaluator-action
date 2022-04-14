0cd79db86b49a8a0dcd4668a5b18b1497de68e8a
Adriel Bento <adrielbb15@gmail.com>
2022-04-14 16:34:44 -0300

    feat: exec intrumented

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const { runStepsEvaluator } = require('./src/controller/evaluator')
-const { processingOutputTests } = require('./src/controller/parserOutput')
 const core = require('@actions/core');
-const testPath = core.getInput('test-path', { required: true });
-const unitTestOutput = core.getInput('unit-test-output', { required: false });
-const instrumentedTestOutput = core.getInput('instrumented-test-output', { required: false });
-
+const unitPath = 'app/build/outputs/androidTest-results/connected/'
+const instrumentedPath = 'app/build/outputs/androidTest-results/connected/'
 
 core.info('\u001b[38;5;6m[info] ⚙️ Rodando avaliador');
 
-testPath ? runStepsEvaluator(testPath) : processingOutputTests(unitTestOutput, instrumentedTestOutput)
+runStepsEvaluator(unitPath, instrumentedPath)

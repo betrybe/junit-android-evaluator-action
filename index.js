3741370086313daf049a915b8b392f98d1685710
katiacih <katiacih@gmail.com>
2022-04-18 13:42:18 -0300

    refactor: corrigindo path test unitario

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { runStepsEvaluator } = require('./src/controller/evaluator')
 const core = require('@actions/core');
-const unitPath = 'app/build/outputs/androidTest-results/connected/'
+const unitPath = 'app/build/test-results/testReleaseUnitTest/'
 const instrumentedPath = 'app/build/outputs/androidTest-results/connected/'
 
 core.info('\u001b[38;5;6m[info] ⚙️ Rodando avaliador');

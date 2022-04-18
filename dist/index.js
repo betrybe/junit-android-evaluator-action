42d5cc8ef677972be71a2b9b93ad7e8a72ff727c
Adriel Bento <adrielbb15@gmail.com>
2022-04-18 15:48:29 -0300

    debug: add log

--- dist/index.js
+++ dist/index.js
@@ -8776,7 +8776,7 @@ const { runStepsEvaluator } = __nccwpck_require__(1941)
 const core = __nccwpck_require__(6964);
 const unitPath = 'app/build/test-results/testReleaseUnitTest/'
 const instrumentedPath = 'app/build/outputs/androidTest-results/connected/'
-const username = core.getInput('pr_author_username', { required: true });
+// const username = core.getInput('pr_author_username', { required: true });
 
 core.info(`\u001b[38;5;6m[info] 🏃‍♂️ Rodando avaliador -> ${username} `);
 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { runStepsEvaluator } = require('./src/controller/evaluator')
 const core = require('@actions/core');
 const unitPath = 'app/build/test-results/testReleaseUnitTest/'
 const instrumentedPath = 'app/build/outputs/androidTest-results/connected/'
-const username = core.getInput('pr_author_username', { required: true });
+// const username = core.getInput('pr_author_username', { required: true });
 
 core.info(`\u001b[38;5;6m[info] 🏃‍♂️ Rodando avaliador -> ${username} `);
 
